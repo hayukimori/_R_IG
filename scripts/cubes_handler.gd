1d274a65842a46e9eed0f7e093504de526180a36
hayukimori <hayukimori@gmail.com>
2025-07-05 02:13:23 -0300

    New cubes handler feature, it will generate cubes using json objects from API (Test API)

--- scripts/cubes_handler.gd
+++ scripts/cubes_handler.gd
@@ -1,7 +1,15 @@
 extends Node
 
+@export_category("3D")
 @export var cluster: Node3D
+@export var user_cube_scene: PackedScene
 @export var gen_cubes_request_url: String = "http://localhost/getcubes"
+
+@export_category("UI")
+@export var devel_ui: DevelopmentUI
+
+
+var cubes_count: int = 0
 
 func _ready() -> void:
 	var content = await request_cubes()
@@ -11,9 +19,43 @@ func _ready() -> void:
 # This function will be replaced in future versions
 func generate_cubes(cubes_array: Array) -> void:
 	if cubes_array != []:
-		push_warning("This function is not implemented yet")
+		for cube in cubes_array:
+			await get_tree().process_frame
+			new_cube(cube)
 	else:
 		push_error("cubes_array is empty.")
+
+
+func new_cube(cube_data: Dictionary) -> void:
+	if user_cube_scene == null:
+		push_error("User cube scene is null")
+		return
+	
+	if cluster == null:
+		push_error("Cluster is null")
+		return
+
+	var cid: String = cube_data.id
+	var owner_id: String = cube_data.owner_id
+	var cube_position: Vector3 = Vector3(
+		cube_data.position_x,
+		cube_data.position_y,
+		cube_data.position_z
+	)
+
+	var t_cube: UserCube = user_cube_scene.instantiate()
+	t_cube.cube_id = cid
+	t_cube.user_id = owner_id
+
+	cluster.add_child(t_cube)
+	t_cube.position = cube_position
+
+	cubes_count += 1
+	
+	# DEBUG
+	devel_ui.update_cubes_count(cubes_count)
+	devel_ui.update_current_json_content(cube_data)
+
 
 # Request for gen_cubes_request_url (GET) to get an array from all existing cubes
 # This function is temporary, and it will be replaced in future versions
@@ -23,13 +65,16 @@ func request_cubes() -> Array:
 
 	var result: Array = []
 
-	http_request.request_completed.connect(func(_result_code, response_code, _headers, body):
+	http_request.request_completed.connect(func(result_code, response_code, _headers, body):
+		print_debug(result_code)
 		if response_code == 200:
 			var json = JSON.parse_string(body.get_string_from_utf8())
 			if typeof(json) == TYPE_ARRAY:
 				result.assign(json)
 			elif typeof(json) == TYPE_DICTIONARY:
 				result.append(json)
+		else:
+			push_error("Server response code: %d" % response_code)
 	)
 
 	http_request.request(gen_cubes_request_url)
