2e799ef4dcb9b688b5eeb7f07291f809455039c2
hayukimori <hayukimori@gmail.com>
2025-07-25 00:55:03 -0300

    feat: Enhance user profile prototype with additional UI elements and data handling

--- scripts/components/prototype/user_profile_prototype.gd
+++ scripts/components/prototype/user_profile_prototype.gd
@@ -1,8 +1,18 @@
 extends Control
 class_name UserProfileUIPrototype
 
+@export_group("API")
+## Profile_ID should set in script
 @export var profile_id: String
+
+## `profile_getter_url` is an url to get profile data (currently it's set to search by id (uid))
 @export var profile_getter_url: String = "http://localhost:3000/api/v1/profile/uid/%s"
+
+# Basic Scene Objects
+@onready var displayNameLabel: Label = $BgPanel/DisplayNameLabel
+@onready var usernameLabel: Label = $BgPanel/UsernameLabel
+@onready var idLabel: Label = $BgPanel/IDLabel
+
 
 var headers: Array = [
 	"Authorization: Bearer %s" % CurrentUserSession.login_token,
@@ -10,22 +20,44 @@ var headers: Array = [
 	"Accept: application/json"
 ]
 
+
+#region Interfaces and Validations
+class UserProfile:
+	var id: String
+	var username: String
+	var displayName: String
+	var bio: String
+	var avatarUrl: String
+	var bannerColor: String
+	var followersCount: int
+	var followingCount: int
+	var isFollowing: bool
+	var badgeIds: Array
+	var links: Array
+
+	func initializeData(data: Dictionary) -> void:
+		self.id = data.get('id', '')
+		self.username = data.get('username', '')
+		self.displayName = data.get('displayName', '')
+		self.bio = data.get('bio', '')
+		self.avatarUrl = data.get('avatarUrl', '')
+		self.bannerColor = data.get('bannerColor', '')
+		self.followersCount = data.get('followersCount', 0)
+		self.followingCount = data.get('followingCount', 0)
+		self.isFollowing = data.get('isFollowing', false)
+		self.badgeIds = data.get('badgeIds', [])
+		self.links = data.get('links', [])
+
 # TODO: Extract Validations to a global tools script.
 class Validations:
 	func validate_uid(uid: String) -> bool:
 		# Early return method
 		# Requirement: UID Needs to be exactly 24 characters long. (12 bytes)
-		if not type_string(typeof(uid)): return false
+		if !type_string(typeof(uid)): return false
 		if uid == "": return false
 		if uid.length() != 24: return false
 
 		return true
-
-func _ready() -> void:
-	await get_tree().process_frame
-
-	if prepareRequirements():
-		loadProfile()
 
 func prepareRequirements() -> bool:
 	var c_valids = Validations.new()
@@ -33,9 +65,32 @@ func prepareRequirements() -> bool:
 
 	return uid_is_valid
 
+#endregion
 
 func loadProfile() -> void:
-	pass
+	var data = await _request_profile()
+	if data.has("error"):
+		print_debug("Got error at data", data) # TODO: Handle Errors
+	
+	var	profileData: UserProfile = UserProfile.new()
+	profileData.initializeData(data)
+
+	updateUI(profileData)
+
+#region UI Functions
+func updateUI(profile_data: UserProfile) -> void:
+	displayNameLabel.text = profile_data.displayName
+	usernameLabel.text = profile_data.username
+	idLabel.text = profile_data.id
+#endregion
+
+#region underscore functions
+func _ready() -> void:
+	await get_tree().process_frame
+
+	if prepareRequirements():
+		loadProfile()
+
 
 func _request_profile() -> Dictionary:
 	var http_request := HTTPRequest.new()
@@ -76,3 +131,8 @@ func _request_profile() -> Dictionary:
 	http_request.queue_free()
 
 	return result
+#endregion
+
+#region Button Signals
+func _on_close_profile_button_pressed() -> void:
+	queue_free()
