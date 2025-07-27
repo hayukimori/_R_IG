139f79ef45aa51f0f2e70768552f56a8a8b27f67
hayukimori <hayukimori@gmail.com>
2025-07-27 00:02:49 -0300

    feat(UI): - Added updateUI logic - Added "user main interface" to main scene

--- scripts/user_main_interface.gd
+++ scripts/user_main_interface.gd
@@ -10,9 +10,55 @@ func _ready() -> void:
 	if !CurrentUserSession.logged_in:
 		push_warning("User is not logged in, profile won't be loaded")
 		return
+	
+	updateUI()
 
 
-func load_profile() -> void:
-	# Get user basic info from current session
-	username_label.text = CurrentUserSession.username
+func updateUI() -> void:
+	var uid = CurrentUserSession.user_id
+
+	# Validates uid
+	if not GeneralTools.Validations.new().validate_uid(uid):
+		push_warning("Invalid UID: %s" % uid)
+		return
+	
+	# Gets profile data (as UserProfile)
+	var profile_data: GeneralTools.UserProfile = await loadProfile(uid)
+
+	# Check for profile
+	if not profile_data:
+		push_warning("Failed to load profile data for UID: %s" % uid)
+		return
+
+	# Updates UI
+	username_label.text = profile_data.username
+
+	# Checks for profile picture
+	if profile_data.avatarUrl != "":
+		var texture = await GeneralTools.getUserPfp(profile_data)
+		profile_picture_trd.texture = texture
+
+	else:
+		var default_image = Image.new()
+		default_image.load(GeneralTools.default_profile_picture)
+		var texture = ImageTexture.create_from_image(default_image)
+		profile_picture_trd.texture = texture
+
+
+
+func loadProfile(profile_id) -> GeneralTools.UserProfile:
+	# Request profile data using id
+	var data = await GeneralTools.requestProfile(profile_id)
+
+	# Verify errors
+	if data.has("error"):
+		print_debug("Got error at data", data)
+	
+	# Gets profile as UserProfile (primitive interface using class)
+	var	profileData: GeneralTools.UserProfile = GeneralTools.UserProfile.new()
+	profileData.initializeData(data)
+
+	return profileData
+
+	
 
