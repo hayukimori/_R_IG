215daea0292cb083b5f02fddcb01fda31f2ab394
hayukimori <hayukimori@gmail.com>
2025-08-03 21:40:42 -0300

    feat(core): Created new Route global script for storing routes and related functions

---
+++ autoloads/Routes.gd
@@ -0,0 +1,15 @@
+extends Node
+
+const ENDPOINT_LOGIN        := "/api/auth/login"
+const ENDPOINT_REGISTER     := "/api/auth/register"
+const ENDPOINT_PING         := "/api/v1/ping"
+const ENDPOINT_GET_PROFILE  := "/api/v1/profile/uid/%s"
+const ENDPOINT_SEND_PROFILE := "/api/v1/editprofile"
+const ENDPOINT_SEND_PICTURE := "/api/v1/update-pfp"
+const ENDPOINT_CUBES        := "/api/v1/cubes"
+const ENDPOINT_NEW_CUBES    := "/api/v1/checknewcubes"
+const ENDPOINT_WORLD_FOLLOWS:= "/api/v1/worldfollows"
+const ENDPOINT_FOLLOW       := "/api/v1/follow"
+const ENDPOINT_UNFOLLOW     := "/api/v1/unfollow"
+const ENDPOINT_FL_EXISTS    := "/api/v1/follow-exists"
+
